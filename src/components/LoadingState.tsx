264bce52dc1083d32292828c73f734953428c3f5
james-eo <jamesejike04@gmail.com>
2026-05-24 05:48:18 +0100

    feat:  add loading state

---
+++ src/components/LoadingState.tsx
@@ -0,0 +1,7 @@
+export default function LoadingState() {
+  return (
+    <div className="mt-6 rounded-xl border border-gray-200 bg-gray-50 p-4 text-center text-gray-600">
+      Generating interview questions...
+    </div>
+  );
+}
